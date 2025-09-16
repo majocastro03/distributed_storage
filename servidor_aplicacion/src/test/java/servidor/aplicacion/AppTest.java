06bd6a348e5e3196bcaf4ec3a3d868529e2f3685
Maria Jose <maria.romero@softlogyco.com>
2025-09-16 14:14:45 -0500

    Initialize Maven project structure with App and AppTest classes

---
+++ servidor_aplicacion/src/test/java/servidor/aplicacion/AppTest.java
@@ -0,0 +1,20 @@
+package servidor.aplicacion;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
